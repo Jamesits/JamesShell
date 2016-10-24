f76b2d7c16864dbff06a30bd36c206e9218a7327
James Swineson <i@swineson.me>
2016-10-24 17:52:00 +0800

    Some new header files

---
+++ src/config.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define SHELL_NAME "JamesShell"
+#define SHELL_DESCRIPTION "A simple and naive shell implementation by James Swineson."
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_REVISION 1
+
+#define DEFAULT_SHELL_PROMPT "Jsh> "
+
+#define READ_BUF_SIZE 1024
+#define TOKEN_SPLIT_BUF_SIZE 16
+#define TOKEN_DELIMETERS " \t\r\n"
+
+#endif /* end of include guard: __CONFIG_H__ */
---
+++ src/main.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+extern shared_config config;
+
+#endif /* end of include guard: __MAIN_H__ */
